65e7111c72531ac03398981220e77ccea07dfe84
umbrik <idumbrik@gmail.com>
2025-03-29 21:35:33 +0100

    Add eslint ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,14 @@
 
 import eslint from "@eslint/js";
+import { globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  [
+    globalIgnores(["build/*"])
+  ],
   {
     rules: {
       "@typescript-eslint/no-wrapper-object-types": "off",
